741c603a7022a1aadc70b61b8f7952d38e8ea90d
Ikechukwu Chiemelie Charles <chiemeliecharleschristopher@gmail.com>
2025-01-06 06:50:09 +0100

    implemented name display

--- data.js
+++ data.js
@@ -1,111 +1,4 @@
 "use strict";
-// const fname1 = document.querySelector("#omo");
-// const fname2 = document.querySelector("#cheta");
-// const fname3 = document.querySelector("#sarah");
-// const fname4 = document.querySelector("#udeme");
-
-// const test1 = document.querySelector(".omo");
-// const test2 = document.querySelector(".cheta");
-// const test3 = document.querySelector(".sarah");
-// const test4 = document.querySelector(".udeme");
-
-// const top1 = document.querySelector(".top1");
-// const top2 = document.querySelector(".top2");
-// const top3 = document.querySelector(".top3");
-// const top4 = document.querySelector(".top4");
-
-const fnames = document.querySelectorAll(".fname");
-
-fnames.forEach(function (fname, i) {
-  fname.addEventListener("click", function () {
-    // console.log(this);
-    const tests = document.querySelectorAll(".test");
-    tests.forEach(function (test) {
-      test.classList.remove("hide");
-      console.log("clicked");
-    });
-    // tests.forEach(function (others) {
-    tests[i].classList.add("hide");
-    console.log("clicked2");
-
-    // });
-    // fnames[i].classList.toggle("activeName");
-    // fnames[i].classList.remove("activeName");
-  });
-});
-
-// fname1.addEventListener("click", function () {
-//   test1.classList.remove("hide");
-//   test2.classList.add("hide");
-//   test3.classList.add("hide");
-//   test4.classList.add("hide");
-
-//   fname1.classList.remove("inActive");
-//   fname1.classList.add("activeName");
-
-//   fname2.classList.remove("activeName");
-//   fname3.classList.remove("activeName");
-//   fname4.classList.remove("activeName");
-
-//   top1.classList.remove("hide");
-//   top2.classList.add("hide");
-//   top3.classList.add("hide");
-//   top4.classList.add("hide");
-// });
-// fname2.addEventListener("click", function () {
-//   test2.classList.remove("hide");
-//   test1.classList.add("hide");
-//   test3.classList.add("hide");
-//   test4.classList.add("hide");
-
-//   fname2.classList.remove("inActive");
-//   fname2.classList.add("activeName");
-
-//   fname1.classList.remove("activeName");
-//   fname3.classList.remove("activeName");
-//   fname4.classList.remove("activeName");
-
-//   top2.classList.remove("hide");
-//   top1.classList.add("hide");
-//   top3.classList.add("hide");
-//   top4.classList.add("hide");
-// });
-// fname3.addEventListener("click", function () {
-//   test3.classList.remove("hide");
-//   test1.classList.add("hide");
-//   test2.classList.add("hide");
-//   test4.classList.add("hide");
-
-//   fname3.classList.remove("inActive");
-//   fname3.classList.add("activeName");
-
-//   fname1.classList.remove("activeName");
-//   fname2.classList.remove("activeName");
-//   fname4.classList.remove("activeName");
-
-//   top3.classList.remove("hide");
-//   top1.classList.add("hide");
-//   top2.classList.add("hide");
-//   top4.classList.add("hide");
-// });
-// fname4.addEventListener("click", function () {
-//   test4.classList.remove("hide");
-//   test1.classList.add("hide");
-//   test2.classList.add("hide");
-//   test3.classList.add("hide");
-
-//   fname4.classList.remove("inActive");
-//   fname4.classList.add("activeName");
-
-//   fname1.classList.remove("activeName");
-//   fname2.classList.remove("activeName");
-//   fname3.classList.remove("activeName");
-
-//   top4.classList.remove("hide");
-//   top2.classList.add("hide");
-//   top3.classList.add("hide");
-//   top1.classList.add("hide");
-// });
 
 const flagDrop = document.querySelector(".flagDrop");
 const flagMenu = document.querySelector(".flagMenu");
@@ -142,4 +35,68 @@ const mobile = document.querySelector(".mobile");
 harm.addEventListener("click", function () {
   mobile.classList.toggle("hidden");
   console.log("click");
+});
+
+const fname = document.querySelectorAll(".fname");
+const namedisplay = document.querySelector(".name");
+const job = document.querySelector(".job");
+const test = document.querySelector(".test");
+
+const names = [
+  "Omolara Gbademosi",
+  "Cheta Odigbo",
+  "Sarah Ladipo",
+  "Udeme Udoh",
+];
+
+const jobs = ["Nurse", "Businessman", "Business developer"];
+
+const texts = [
+  ` When ASUU called off the strike and my daughter was to go back to
+              school, the money I was expecting wasn't forthcoming and my
+              daughter is about to graduate. It’s FairMoney loans that helped
+              us. I started a new business in school. It’s been going well, but
+              I needed some money to buy things for this season. Can’t ask my
+              parents because things are tight. I eventually used FairMoney
+              loans, and my business has been growing.`,
+
+  ` My favourite thing about my FairMoney account is the free transfer
+              because I do a lot of transfers around salary period. When I’m now
+              balancing my account, it won’t balance again because of the
+              transfer charge. Never again. The thing I like is the all-in-one
+              ness. Once money enters my account, I can easily transfer some to
+              FairSave or FairLock. No excuses, no stress of transferring out.
+              Everything is in one place.`,
+  `    I remember when I got my first 2% cash back. I thought it was a
+              debit. I wanted to fight. When I realised it was actually a
+              credit, I grinned a lot. It was small, but I liked it. The biggest
+              reason I like your cards is that I can pay for my Netflix
+              comfortably. Before, I used my friend's card. For some reason, my
+              other cards just don’t work. Now, I use my FairMoney card
+              conveniently.`,
+  ` Am limited to giving you 5 star FairMoney. You are my best app
+              presently on my phone. App that gives me daily interest
+              (FairSave), app that gives highest interest on my savings
+              (Fairlock), App that gives me money when am broke (Fairmoney
+              loan), App that gives me discount in my airtime and data purchase.
+              Who dey breath!!! Fairmoney app- making life convenient; is just a
+              click away. I love you and there's nothing you can do about it.`,
+];
+
+fname.forEach(function (name, i) {
+  name.addEventListener("click", function (e) {
+    console.log(e.target);
+
+    fname.forEach(function (c) {
+      c.classList.remove("activeName");
+    });
+
+    name.classList.add("activeName");
+
+    names[i];
+    namedisplay.textContent = this.textContent;
+
+    job.textContent = jobs[i];
+    test.textContent = texts[i];
+  });
 });
